836932eacf2277424726430e44b8395c23d4bdf1
Ashish Gautam (HCL TECHNOLOGIESCORPORATESERVI) <v-ashga@microsoft.com>
2023-03-21 18:50:00 +0530

    increase text area size

--- tabs/src/components/capabilities/App.tsx
+++ tabs/src/components/capabilities/App.tsx
@@ -1,4 +1,4 @@
-import { Button, TextArea } from "@fluentui/react-northstar";
+import { Button, Flex, TextArea } from "@fluentui/react-northstar";
 
 import { app } from "@microsoft/teams-js";
 import { booleanToString } from "../../helpers";
@@ -14,19 +14,19 @@ export const App = () => {
         });
 
         // return button to get context and displays the information/ context in the resizeable text area
-        return (<>
-            <Button onClick={async () => {
-                const context = await app.getContext();
-                const contextString = JSON.stringify(context);
-                setText(contextString);
-                setShowText(true);
-                console.log(context);
-            }}>
-                Get Context
-            </Button>
-            {showText &&
-                <TextArea resize="horizontal" value={text} />}
-        </>
+        return (
+            <Flex gap="gap.small" vAlign="center">
+                <Button className="ui_context" onClick={async () => {
+                    const context = await app.getContext();
+                    const contextString = JSON.stringify(context);
+                    setText(contextString);
+                    setShowText(true);
+                    console.log(context);
+                }}> Get Context
+                </Button>
+                {showText &&
+                    <TextArea fluid inverted value={text} />}
+            </Flex>
         )
     }
     // return empty fragment if app has not been initialized
